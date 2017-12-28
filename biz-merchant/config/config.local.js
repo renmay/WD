de8610ed6144380a49bd60f9f422f68bb56d145e
renmay <renmay@qq.com>
2017-12-28 21:20:48 +0800

    优化界面

--- biz-merchant/config/config.local.js
+++ biz-merchant/config/config.local.js
@@ -1,33 +1,33 @@
 module.exports = app => {
-    const exports = {
+  const exports = {
 
-    };
+  };
 
-    exports.api = {
-        base: 'http://127.0.0.1:8080',
-        common: 'http://common.zzyuu.com',
-        static: '',
-        image: 'http://img-test.zzyuu.com'
-    };
+  exports.api = {
+    base: 'http://111.230.45.77:8080',
+    common: 'http://common.zzyuu.com',
+    static: '',
+    image: 'http://img-test.zzyuu.com',
+  };
 
-    exports.static = {
-        prefix: ''
-    };
+  exports.static = {
+    prefix: '',
+  };
 
-    exports.redis = {
-        client: {
-            host: '120.79.0.38',
-            db: 5,
-            port: 6379,
-            password: 'Wd123456'
-        }
-    };
+  exports.redis = {
+    client: {
+      host: '120.79.0.38',
+      db: 5,
+      port: 6379,
+      password: 'Wd123456',
+    },
+  };
 
-    exports.development = {
-        watchDirs: ['/app/data'],
-        ignoreDirs: [],
-        fastReady: true,
-    };
+  exports.development = {
+    watchDirs: [ '/app/data' ],
+    ignoreDirs: [],
+    fastReady: true,
+  };
 
-    return exports;
+  return exports;
 };
